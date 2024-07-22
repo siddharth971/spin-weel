ec10f8a9b76b66438b53e6ddc441a0df43d241e8
siddharth971 <siddharthsiddharth971@gmail.com>
2024-07-22 14:08:21 +0530

    Changes to be committed:

--- script.js
+++ script.js
@@ -1,14 +1,11 @@
-const { plugins } = require("chart.js");
-
 const wheel = document.getElementById("wheel");
 const spinBtn = document.getElementById("spin-btn");
-
 const finalValue = document.getElementById("final-value");
 
 
 //object that stores of minimum and maximum angle of a value
 
-const retationValue = [
+const roValue = [
     { minDegree: 0, maxDegree: 30, value: 2 },
     { minDegree: 31, maxDegree: 90, value: 1 },
     { minDegree: 91, maxDegree: 150, value: 6 },
@@ -25,7 +22,7 @@ const data = [16, 16, 16, 16, 16, 16];
 
 //background color for each piace
 
-let colorCodes = ['#FF5733', '#33FF57', '#5733FF', '#FFFF33', '#33FFFF', '#FF33FF'];
+let colorCodes = ['#2b0c44', '#500f5f', '#580746', '#210436', '#2d084e', '#340436'];
 
 //create chart 
 
@@ -58,9 +55,36 @@ let myChart = new Chart(wheel, {
                 color: "white",
                 formatter: (_, context) =>
                     context.chart.data.labels[context.dataIndex],
-                font: { size: 24 },
+                font: { size: 24 }
 
             }
         }
     }
 });
+
+//display value button
+
+const valueGenerater = (angeValue) => {
+    for (let i of roValue) {
+        //if the angle value is between min and max then display it 
+        if (angeValue >= i.maxDegree && angeValue <= i.maxDegree) {
+            finalValue.innerHTML = `<p>Value: ${i.value}</p>`;
+            spinBtn.disabled = false;
+            break;
+        }
+    }
+}
+
+
+//spinner count 
+let count = 0;
+
+//100 rotation result for animation and last rotation result
+
+
+let resultValue = 101;
+//Start spinning
+spinBtn.addEventListener("click", () => {
+    spinBtn.disabled = true;
+    //empty final value
+})
